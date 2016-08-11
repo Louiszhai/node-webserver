b3968aa6ff171c94765fd446e43ff52f899b486b
Louiszhai <kingszhai@gmail.com>
2016-08-11 11:22:55 +0800

    修改数组方式配置为对象方式配置

--- index.js
+++ index.js
@@ -1,43 +1,52 @@
 var app = require('express')(),
 	http = require('http').Server(app);
 //*************************** get ***************************
-var getHandlers = [
-	['/',  {
+var getHandlers = {
+	'/': {
 		getKey : 'Welcome to simple node  WebServer!'
-	}]
-];
-getHandlers.forEach(function (handler) {
-	app.get(handler[0], function(req, res) {
-		res.writeHead(200,{
-			'Content-Type': 'text/json'
-		});
-		res.write(JSON.stringify(handler[1]));
-		res.end();
-	});
-});
+	}
+};
+
+for(var path in getHandlers){
+	if(getHandlers.hasOwnProperty(path)){
+		(function(path){
+			app.get(path, function(req, res) {
+				res.writeHead(200,{
+					'Content-Type': 'text/json'
+				});
+				res.write(JSON.stringify(getHandlers[path]));
+				res.end();
+			});
+		})(path);
+	}
+}
 //*************************** get ***************************
 
 //*************************** post ***************************
-var postHandlers = [
-	['/test', {
+var postHandlers = {
+	'/test': {
 		postKey : 'post测试数据'
-	}]
-];
+	}
+};
 
-postHandlers.forEach(function (handler) {
-	app.post(handler[0], function(req, res) {
-		res.writeHead(200,{
-			'Content-Type': 'text/json',
-			'Access-Control-Allow-Credentials': true,
-			'Access-Control-Allow-Headers': 'x-requested-with,Content-Type',
-			'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
-			'Access-Control-Allow-Origin': req.headers.origin,
-			'Access-Control-Max-Age': 3600
-		});
-		res.write(JSON.stringify(handler[1]));
-		res.end();
-	});
-});
+for(path in postHandlers){
+	if(postHandlers.hasOwnProperty(path)) {
+		(function(path){
+			app.post(path, function(req, res) {
+				res.writeHead(200,{
+					'Content-Type': 'text/json',
+					'Access-Control-Allow-Credentials': true,
+					'Access-Control-Allow-Headers': 'x-requested-with,Content-Type',
+					'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
+					'Access-Control-Allow-Origin': req.headers.origin,
+					'Access-Control-Max-Age': 3600
+				});
+				res.write(JSON.stringify(postHandlers[path]));
+				res.end();
+			});
+		})(path);
+	}
+}
 //*************************** post ***************************
 
 http.listen(10108, function(){
