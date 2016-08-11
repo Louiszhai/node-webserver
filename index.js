fcf07636ea3bcd3edab1f5010689c6b4b0e47cc4
Louiszhai <kingszhai@gmail.com>
2016-08-11 11:41:21 +0800

    get请求支持跨域&&整理数据结构

--- index.js
+++ index.js
@@ -1,53 +1,48 @@
 var app = require('express')(),
 	http = require('http').Server(app);
-//*************************** get ***************************
-var getHandlers = {
-	'/': {
-		getKey : 'Welcome to simple node  WebServer!'
+
+//*************************** handler ***************************
+var handlers = {
+	'get': {
+		'/': {
+			getKey : 'Welcome to simple node  WebServer!'
+		}
+	},
+	'post': {
+		'/test': {
+			postKey : 'post测试数据'
+		}
 	}
 };
 
-for(var path in getHandlers){
-	if(getHandlers.hasOwnProperty(path)){
-		(function(path){
-			app.get(path, function(req, res) {
-				res.writeHead(200,{
-					'Content-Type': 'text/json'
+function doHandler(method, handle){
+	for(var path in handle){
+		if(handle.hasOwnProperty(path)){
+			(function(path){
+				app[method](path, function(req, res) {
+					console.log(req.headers);
+					res.writeHead(200,{
+						'Content-Type': 'text/json',
+						'Access-Control-Allow-Credentials': true,
+						'Access-Control-Allow-Headers': 'x-requested-with,Content-Type',
+						'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
+						'Access-Control-Allow-Origin': req.headers.origin || req.headers.host,
+						'Access-Control-Max-Age': 3600
+					});
+					res.write(JSON.stringify(handle[path]));
+					res.end();
 				});
-				res.write(JSON.stringify(getHandlers[path]));
-				res.end();
-			});
-		})(path);
+			})(path);
+		}
 	}
 }
-//*************************** get ***************************
 
-//*************************** post ***************************
-var postHandlers = {
-	'/test': {
-		postKey : 'post测试数据'
-	}
-};
-
-for(path in postHandlers){
-	if(postHandlers.hasOwnProperty(path)) {
-		(function(path){
-			app.post(path, function(req, res) {
-				res.writeHead(200,{
-					'Content-Type': 'text/json',
-					'Access-Control-Allow-Credentials': true,
-					'Access-Control-Allow-Headers': 'x-requested-with,Content-Type',
-					'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
-					'Access-Control-Allow-Origin': req.headers.origin,
-					'Access-Control-Max-Age': 3600
-				});
-				res.write(JSON.stringify(postHandlers[path]));
-				res.end();
-			});
-		})(path);
+for(var method in handlers) {
+	if (handlers.hasOwnProperty(method)) {
+		doHandler(method, handlers[method]);
 	}
 }
-//*************************** post ***************************
+//*************************** handler ***************************
 
 http.listen(10108, function(){
 	console.log('listening on *:10108');
